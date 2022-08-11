ee1277e7d441ecc264e4aed497efcb26b3c36e52
Veer Singh <veersingh209@icloud.com>
2022-08-11 14:12:38 -0700

    Initial commit

---
+++ PhotoGallery/PhotoGallery/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PhotoGallery
+//
+//  Created by Veer Singh on 8/11/22.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .navigationBarHidden(true)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoGallery/PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Veer Singh on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnBoardingView()
+        }
+    }
+}
